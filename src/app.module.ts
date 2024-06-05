882e6438aa7b5a5d666cac4a40d90f835059b776
Marco Antônio <marco.netto@mjv.com.br>
2024-06-04 23:58:00 -0300

    feat: :sparkles: alterando endpoint inicial para redirecionar para a documentação swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlockChainModule } from './block-chain/block-chain.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -9,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot('mongodb://localhost:27017/blockchain'),
     BlockChainModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.use('/', (req, res) => {
+    res.redirect('/api');
+  });
+
   await app.listen(3000);
 }
 bootstrap();
